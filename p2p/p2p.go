f791ae699d0296fc57e132c691c841fc26fef0be
GeonHyeok-Lee <geonhyeoklee.kr@gmail.com>
2021-08-08 19:49:38 +0900

    :feat Add writing message function with websocket

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,7 +3,6 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/GeonHyeok-Lee/cryptocurrency/utils"
 	"github.com/gorilla/websocket"
@@ -22,16 +21,11 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	conns = append(conns, conn)
 	utils.HandleErr(err)
-
 	initPeer(conn, ip, openPort)
-	time.Sleep(20 * time.Second)
-	conn.WriteMessage(websocket.TextMessage, []byte("Hello from port 3000!"))
 }
 
 func AddPeer(address, port, openPort string) {
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
 	initPeer(conn, address, port)
-	time.Sleep(10 * time.Second)
-	conn.WriteMessage(websocket.TextMessage, []byte("Hello from port 4000!"))
 }
